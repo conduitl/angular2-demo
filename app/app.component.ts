f90cd711070939202e2ab7eb9fdc26acc4ebe830
Thomas <thomas.rundle@conduitlearning.com>
2016-07-30 12:38:34 -0500

    Added .container for app view and columns for heroes view

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,14 +6,7 @@ import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{ title }}</h1>
-        <nav>
-            <a [routerLink]="['/dashboard']" routerLinkActive="active">Dashboard</a>
-            <a [routerLink]="['/heroes']" routerLinkActive="active">Heroes</a>
-        </nav>
-        <router-outlet></router-outlet>
-        `,
+    templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css'],
     directives: [ROUTER_DIRECTIVES],
     providers: [HeroService]
